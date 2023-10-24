a4771b4ba451dc49f7bae4b47b111471d6b81b00
nuta <nuta_bogda@yahoo.com>
2023-10-24 11:51:15 +0300

    First upload

---
+++ src/main/java/com/example/design_patterns_sp_nbg/DesignPatternsSpNbgApplication.java
@@ -0,0 +1,13 @@
+package com.example.design_patterns_sp_nbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsSpNbgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsSpNbgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/design_patterns_sp_nbg/DesignPatternsSpNbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.design_patterns_sp_nbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsSpNbgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
